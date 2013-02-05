2d7a02f7bad7068eaa595ac8a9881454584f68f9
ikasam_a <masaki.nakagawa@gmail.com>
2013-02-05 13:17:29 +0900

    initial commit

---
+++ lib/UNIVERSAL/send.pm
@@ -0,0 +1,37 @@
+package UNIVERSAL::send;
+
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+$VERSION = eval $VERSION;
+
+1;
+
+=head1 NAME
+
+UNIVERSAL::send - A module that ...
+
+=head1 SYNOPSIS
+
+    use UNIVERSAL::send;
+
+=head1 DESCRIPTION
+
+UNIVERSAL::send is
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+NAKAGAWA Masaki E<lt>masaki@cpan.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
